eb64f7a965191d5433f97317239549b5d91b3c09
GJ Wang <itellboy@foxmail.com>
2019-08-27 12:01:18 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  base: '/',
+  title: 'JavaScript Webapi',
+  description: 'Love what you do, do what you love. :)',
+  head: [
+    ['link', { ref: 'icon', href: '/svg/js-cube.svg' }],
+  ],
+  host: '0.0.0.0',
+  port: '8080',
+  dest: 'docs/.vuepress/dist',
+  cache: true,
+  extraWatchFiles: [],
+  markdown: {
+    lineNumbers: true,
+  },
+  configureWebpack: (config, isServer) => { },
+  evergreen: true,
+  themeConfig: {
+    sidebar: [
+      ['/welcome', 'welcome'],
+    ],
+  },
+}
---
+++ docs/welcome.md
@@ -0,0 +1,7 @@
+**Hello, guys! Welcome~**
+
+Thanks for you visiting my site, I hope it benifit you.
+
+My name is **Guojun Wang（王国军）**, you can call me guojun. I was born in 1995. I am a software developer focus on **Front End Development**, based **HangZhou** in China.
+
+**HERE IS CATALOG**
